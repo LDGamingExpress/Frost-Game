aeb5861ecaa1c4ab07e5d30a042cd9861a475dde
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2025-02-09 15:54:50 -0500

    CrystalsAndLadder
    Crystals added along with ladder

--- scaling-the-frost/PlayerScript.gd
+++ scaling-the-frost/PlayerScript.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 3.0
 const JUMP_VELOCITY = 3.5
 var push_force = 2.0
+var OnLadder = false
 
 func _input(event): # Checks for input
 	if event is InputEventMouseMotion: # Checks if the input is the mouse moving
@@ -11,12 +12,23 @@ func _input(event): # Checks for input
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if $Camera3D/RayCast3D.is_colliding():
+		if $Camera3D/RayCast3D.get_collider().is_in_group("Notes"):
+			$Camera3D/CanvasLayer/NoteLabel.visible = true
+		else:
+			$Camera3D/CanvasLayer/NoteLabel.visible = false
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if OnLadder == true:
+		if Input.is_action_pressed("Forward"):
+			velocity.y = SPEED
+		if Input.is_action_pressed("Backward"):
+			velocity.y = -SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
